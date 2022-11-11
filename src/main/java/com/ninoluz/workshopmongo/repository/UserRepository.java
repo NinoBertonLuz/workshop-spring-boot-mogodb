58aa9a3dad662df00c0c6f220a522834ba7557a2
Thiago Gonçalves Luz <tgluz@hotmail.com>
2022-11-11 16:10:16 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/ninoluz/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.ninoluz.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ninoluz.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+	
+
+}
