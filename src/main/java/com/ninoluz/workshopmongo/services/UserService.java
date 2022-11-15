f97146d7a4907cfb4a9ce3ae7b5b7c489940da89
Thiago Gonçalves Luz <tgluz@hotmail.com>
2022-11-15 15:45:38 -0300

    Atualização de usuário com PUT

--- src/main/java/com/ninoluz/workshopmongo/services/UserService.java
+++ src/main/java/com/ninoluz/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.ninoluz.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -38,6 +39,19 @@ public class UserService {
 		repo.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		User newObj = repo.findById(obj.getId()).orElse(null);
+		updateData(newObj, obj);
+		return repo.save(newObj);
+		
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+		
+	}
+
 	public User fromDTO(UserDTO objDto){
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
