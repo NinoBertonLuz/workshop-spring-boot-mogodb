e329b794ecf803c53ac962df74469b374c1a205f
Thiago Gonçalves Luz <tgluz@hotmail.com>
2022-11-16 15:39:01 -0300

    Consulta com vários critérios

--- src/main/java/com/ninoluz/workshopmongo/services/PostService.java
+++ src/main/java/com/ninoluz/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.ninoluz.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,5 +27,10 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text){
 		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 *60 *60 *1000); // adiciona mais 24horas à data maxima
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
